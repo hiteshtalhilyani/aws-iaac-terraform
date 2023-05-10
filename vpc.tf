0cd0f319b7abd64085e7a008f177e476c209e2be
Hitesh Talhilyani <hiteshtalhilyani@outlook.com>
2023-05-10 19:04:50 +0400

    removed plugins

--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,23 @@
-module "vpc"{
-    source = "terraform-aws-modules/vpc/aws"
-     
-    name = var.vpc_name
-    cidr = var.vpc_cidr
-    azs =  [var.ZONE1, var.ZONE2, var.ZONE3]
-    private_subnets = [var.priv1_cidr,var.priv2_cidr,var.priv3_cidr]
-    public_subnets = [var.pub1_cidr, var.pub2_cidr, var.pub3_cidr]
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-    enable_nat_gateway   = true
-    single_nat_gateway   = true
+  name            = var.vpc_name
+  cidr            = var.vpc_cidr
+  azs             = [var.ZONE1, var.ZONE2, var.ZONE3]
+  private_subnets = [var.priv1_cidr, var.priv2_cidr, var.priv3_cidr]
+  public_subnets  = [var.pub1_cidr, var.pub2_cidr, var.pub3_cidr]
 
-    tags = {
-        terraform = "true"
-        Environment = "Dev"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 
-    }
-    vpc_tags = {
+  tags = {
+    terraform   = "true"
+    Environment = "Dev"
+
+  }
+  vpc_tags = {
     Name = var.vpc_name
-    }
+  }
 }
